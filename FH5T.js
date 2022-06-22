6f42b8b622bd4b9d5d4e1d8a872ab5c17c797a48
Nereid42 <ako42@gmx.de>
2022-06-22 20:38:21 +0200

    improved http server

--- FH5T.js
+++ FH5T.js
@@ -3,6 +3,13 @@ const http = require("./js/http/http.js");
 
 console.log("FH5 - Telemetry");
 
-http.WWW_ROOT = __dirname+"/html";
-const server = new http.HttpServer(8000);
+
+function test(response) {
+    console.log("test called");
+    response.writeHead(200);
+    response.end("THIS IS A TEST");      
+}
+
+const server = new http.HttpServer(8000, __dirname+"/html");
+server.register("/test",test);
 server.start();
--- js/http/http.js
+++ js/http/http.js
@@ -4,53 +4,77 @@ const { url } = require("inspector");
 const fs = require('fs').promises;
 const path = require('path');
 
-const requestListener = function (req, res) {
-    const url = req.url;
-    const path = exports.WWW_ROOT + req.url; //.replace(/\//g,"\\");
-
-    headerSecFetchDest = req.headers['sec-fetch-dest'];
-
-    // check if file is existent
-    fs.access(path, fs.constants.R_OK).then(
-        () => {
-            // file is existent
-            //
-            // check if css-style is requestet
-            if(headerSecFetchDest == "style") {
-                // set content to css
-                res.setHeader("Content-Type", "text/css");
-            }
-            // check if image is requestet
-            else if(headerSecFetchDest == "image") {
-                // set content to image
-                res.setHeader("Content-Type", "image");
-            }
-            // send text in all other cases
-            else {
-                // set content to text
-                res.setHeader("Content-Type", "text/html");
-            }
-            // read and send file
-            fs.readFile(path).then(contents => {
-                res.writeHead(200);
-                res.end(contents);    
-        })
-    }).catch(() => console.error('file '+path+" cannot be accessed"));  
-
-    if(url=="/STOP") {
-        console.log("STOP received");
-        process.exit();        
-    }
-}
-
-exports.WWW_ROOT = __dirname;
-
 exports.HttpServer = class {
     constructor(port,root) {
+        this.root = root;
         this.port = port;
-        this.server = http.createServer(requestListener);
         this.running = false;
+        this.registeredUrls = {};
+
+        this.server = http.createServer( (request, response) => {
+            const url = request.url;
+    
+            console.log("request "+url);
+
+            var func =  this.registeredUrls[url];
+            
+            if(func !== undefined) {
+                console.log("calling "+func.name);
+                func(response);
+            }
+            else {
+                this.processFileRequest(request, response);
+            }
+        
+                
+            if(url=="/STOP") {
+                console.log("STOP received");
+                process.exit();        
+            }
+        });
     }
+
+    processFileRequest(request, response) {
+        const url = request.url;
+        const path = this.root + request.url;
+
+        console.log("file request "+url);
+    
+        var headerSecFetchDest = request.headers['sec-fetch-dest'];
+
+        // check if file is existent
+        fs.access(path, fs.constants.R_OK).then(
+            () => {
+                // file is existent
+                //
+                // check if css-style is requestet
+                if(headerSecFetchDest == "style") {
+                    // set content to css
+                    response.setHeader("Content-Type", "text/css");
+                }
+                // check if image is requestet
+                else if(headerSecFetchDest == "image") {
+                    // set content to image
+                    response.setHeader("Content-Type", "image");
+                }
+                // send text in all other cases
+                else {
+                    // set content to text
+                    response.setHeader("Content-Type", "text/html");
+                }
+                // read and send file
+                fs.readFile(path).then(contents => {
+                    response.writeHead(200);
+                    response.end(contents);    
+            })
+        }).catch(() => {
+            console.error('file '+path+" cannot be accessed");
+            response.writeHead(404);
+            response.end();    
+});  
+
+    }
+
 
     start() {
         if(!this.running) {
@@ -59,6 +83,10 @@ exports.HttpServer = class {
                 console.log("Server is running on http://127.0.0.1:${this.port}");
             });
         }
+    }
+
+    register(url, func) {
+        this.registeredUrls[url] = func;
     }
 
     toString() {
