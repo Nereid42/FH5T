5629c64700f42b945c6bb108731e1a9eae752e98
Nereid42 <ako42@gmx.de>
2022-06-23 21:19:18 +0200

    data until index 58 verified

--- js/network/udp.js
+++ js/network/udp.js
@@ -8,68 +8,107 @@ exports.Fh5Datagram = class {
 
         this.index=0;
         this.pos=0;
-        // race & timestamp
-        this.readInt(2);
-        // rpm
+        // 0-1 0 race & timestamp
+        this.readInt32(2);
+        // 2-4 8 rpm (current, idle, max)
         this.readFloatAsInt(3);
-        // acceleration
+        // 5-7 20 acceleration (X,Y,Z)
         this.readFloat(3);
-        // velocity
+        // 8-10 32 velocity  (X,Y,Z)
         this.readFloat(3);
-        // angular velocity
+        // 11-13 44 angular velocity  (X,Y,Z)
         this.readFloat(3);
-        // yaw, pitch, roll
+        // 14-16 56 yaw, pitch, roll
         this.readFloat(3);
-        // suspensiontravel
+        // 17-20 68 normalized suspensiontravel
         this.readFloat(4);
-        // tireslip
+        // 21-24 84 tire slip ratio
         this.readFloat(4);
-        // wheel rotation
+        // 25-28 100 wheel rotation
         this.readFloat(4);
-        // wheel on rumble strip
-        this.readInt(4);
-        // wheel in puddle depth
-        this.readInt(4);
-        // surface rumble
+        // 29-32 116 wheel on rumble strip
+        this.readInt32(4);
+        // 33-36 132 wheel in puddle depth
+        this.readInt32(4);
+        // 37-40 148 surface rumble
         this.readFloat(4);
-        // tire slip angle
+        // 41-44 164 tire slip angle
         this.readFloat(4);
-        // suspension travel meters
+        // 45-48 180 tire combined slip
         this.readFloat(4);
-        // car Id, class, performance index
-        this.readInt(3)
-        // drive train type
-        this.readInt(1)
-        // num cylinders
-        this.readInt(1)
-        // car category
-        this.readInt(1)
-        // unknown (crashing)
-        this.readInt(1)
-        // position x, y, z
+        // 49-52 196 suspension travel meters
+        this.readFloat(4);
+        // 53-55 212 car id, car class, car performance index (OK)
+        this.readInt32(3)
+        // 56 224 drive train type
+        this.readInt32(1)
+        // 57 228 num cylinders
+        this.readInt32(1)
+        // 58 232 car category -- V2
+        this.readInt32(1)
+        // 59 236 unknown (crashing)
+        this.readInt32(1)
+        // 60-62 240 position x, y, z
         this.readFloat(3);
+        // 63-65 252 speed, power, torque
+        this.readFloat(3);
+        // 66-69 264 tire temp
+        this.readFloat(4);
+        // 70-71 280 boost, fuel
+        this.readFloat(2);
+        // 72 288 distance traveled
+        this.readFloat(1);
+        // 73-75 292 best lap, last lab, current lap
+        this.readFloat(3);
+        // 76 304 current race time
+        this.readFloat(1);
+        // 77 308 lap numer
+        this.readInt16(1);
+        // 78 310 race position
+        this.readInt8(1);
+        // 79 311 throttle
+        this.readInt8(1);
+        // 80 312 brake
+        this.readInt8(1);
+        // 81 313 clutch
+        this.readInt8(1);
+        // 82 314 handbrake
+        this.readInt8(1);
+        // 83 315 gear
+        this.readInt8(1);
+        // 84-86 316 steer, driving lane, normalized brake diff
+        this.readInt8(3);
+        // 87 319 unknown
+        this.readInt8(1);
+        // 88 320 unknown
+        this.readInt8(1);
+        // 320
+        console.log("end  at "+this.pos);
 
-        // tire combined slip
-        this.readFloat(4);
-        /*var index=0;
-        this.isRaceOn = this.data[index++];
-        this.timestampMS = this.data[index++];
-        this.engineIdleRpm =  this.data[index++];
-        this.engineMaxRpm = this.data[index++];
-        this.currentEngineRpm = this.data[index++];
-        this.accellerationX = this.data[index++];
-        this.accellerationY = this.data[index++];
-        this.accellerationZ = this.data[index++];
-        this.velocityX = this.data[index++];
-        this.velocityY = this.data[index++];*/
+        
+
     }
     
-    readInt(n) {
+    readInt32(n) {
         for(var i=0; i<n; i++) {
             this.data[this.index++] = this.buffer.readInt32LE(this.pos);
             this.pos += 4;
         }
     }
+
+    readInt16(n) {
+        for(var i=0; i<n; i++) {
+            this.data[this.index++] = this.buffer.readInt16LE(this.pos);
+            this.pos += 2;
+        }
+    }    
+
+    readInt8(n) {
+        for(var i=0; i<n; i++) {
+            this.data[this.index++] = this.buffer.readInt8(this.pos);
+            this.pos += 1;
+        }
+    }        
 
     readFloat(n) {
         for(var i=0; i<n; i++) {
