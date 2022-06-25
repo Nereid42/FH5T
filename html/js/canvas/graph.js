6a5337d181706f6111ccef52f7eb5579b4b2e5f1
Nereid42 <ako42@gmx.de>
2022-06-25 18:48:41 +0200

    Debug telegram data on bottom

--- html/js/canvas/graph.js
+++ html/js/canvas/graph.js
@@ -1,7 +1,7 @@
-function fillCanvas(ctx) {
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0, 0, 150, 75);
-}
+const BACKGROUND="#000000";
+const BARCOLOR="#FF0000";
+const ACTIVEBARCOLOR="#9090FF";
+const AXISCOLOR="#9090FF";
 
 
 function drawTelemetryAsBars(telemetry, canvas, max) {
@@ -21,20 +21,21 @@ function drawTelemetryAsBars(telemetry, canvas, max) {
     }
 
     var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#000000";
+    ctx.fillStyle = BACKGROUND;
     ctx.fillRect(0, 0, w, h );
-    ctx.fillStyle = "#FF0000";
+    ctx.fillStyle = BARCOLOR;
     for(var i=0; i<cnt; i++) {
         var x = x0 + i * (dx+gap);
-        var b = dy*telemetry.get(telemetry.size()-cnt+i);
+        var value = telemetry.get(telemetry.size()-cnt+i);
+        var b = dy*value;
         if(i==cnt-1) {
-            ctx.fillStyle = "#9090FF";
+            ctx.fillStyle = ACTIVEBARCOLOR;
         }
         ctx.fillRect(x, y0-b, dx, b );
     }
 
     // scale
-    ctx.fillStyle = "#9090FF";
+    ctx.fillStyle = AXISCOLOR;
     ctx.fillRect(0, y0, w, 2 );    
     var marks = cnt/10;
     var mx = cnt*(dx+gap);
@@ -51,3 +52,48 @@ function drawTelemetryAsBars(telemetry, canvas, max) {
     }
 
 } 
+
+
+function drawTelemetryAsCenteredBars(telemetry, canvas, max) {
+    var w = canvas.width;
+    var h = canvas.height;
+    var dx = 2;
+    var gap = 2;
+    var x0 = 0;
+    var botomMargin = 15;
+    var y0 = h/2;
+    var cnt = Math.min( w / (dx+gap), telemetry.size());
+    if(max > 0) {
+        var dy = y0 / max;
+    }
+    else {
+        var dy = 0;
+    }
+
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = BACKGROUND;
+    ctx.fillRect(0, 0, w, h);
+    ctx.fillStyle = BARCOLOR;
+    for(var i=0; i<cnt; i++) {
+        var x = x0 + i * (dx+gap);
+        var value = telemetry.get(telemetry.size()-cnt+i);
+        var b = dy*value;
+        if(i==cnt-1) {
+            ctx.fillStyle = ACTIVEBARCOLOR;
+        }
+        ctx.fillRect(x, y0-b, dx, b );
+    }
+
+    // scale
+    ctx.fillStyle = AXISCOLOR;
+    ctx.fillRect(0, y0, w, 2 );    
+    // 
+    /*ctx.font = "11px sans serif";
+    var dt = telemetry.getLastTime() - telemetry.getTime(telemetry.size()-cnt);;
+    ctx.fillText(millisAsString(dt),2,h);
+    if(mx >70) {
+      ctx.fillText("00:00",mx-25,h);
+    }*/
+
+} 
+
